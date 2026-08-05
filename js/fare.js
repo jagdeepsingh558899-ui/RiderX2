341e3b4de3ec1cf9e38c5e4d2ed7036a834ce61c
Jagdeep Singh <jagdeepsingh558899@gmail.com>
2026-08-06 05:03:11 +0530

    Create fare.js

---
+++ js/fare.js
@@ -0,0 +1,60 @@
+// =================================
+// RiderX Admin Fare System
+// =================================
+
+
+import {db} from "../firebase/config.js";
+
+
+import {
+
+doc,
+getDoc
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+
+
+
+
+export async function getFareSettings(){
+
+
+let snap = await getDoc(
+
+doc(db,"settings","app")
+
+);
+
+
+
+if(snap.exists()){
+
+
+return snap.data();
+
+
+}
+
+
+
+return {
+
+bikeBase:50,
+
+bikeKm:10,
+
+cabBase:100,
+
+cabKm:15,
+
+offer:0
+
+};
+
+
+}
