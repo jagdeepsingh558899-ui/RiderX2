1ff90575cb6ce847e1ff6c6612da9aa6ef923405
Jagdeep Singh <jagdeepsingh558899@gmail.com>
2026-08-06 00:40:51 +0530

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,39 @@
+// RiderX Firebase Config
+
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import { 
+getAuth 
+} from 
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import { 
+getFirestore 
+} from 
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+
+apiKey: "YOUR_API_KEY",
+
+authDomain: "YOUR_AUTH_DOMAIN",
+
+projectId: "YOUR_PROJECT_ID",
+
+storageBucket: "YOUR_STORAGE_BUCKET",
+
+messagingSenderId: "YOUR_MESSAGING_ID",
+
+appId: "YOUR_APP_ID"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
