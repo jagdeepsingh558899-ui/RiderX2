cccefbddb01b402fac4cc07edadea892d114eef7
Jagdeep Singh <jagdeepsingh558899@gmail.com>
2026-08-06 03:41:24 +0530

    Update leaflet-map.js

--- js/leaflet-map.js
+++ js/leaflet-map.js
@@ -1,47 +1,44 @@
-import { db } from "../firebase/config.js";
+// =================================
+// RiderX Free Map System (Leaflet)
+// =================================
 
 
-import {
+// Initialize Map
 
-ref,
-set,
-onValue
+export function initMap(){
+
+
+let mapElement = document.getElementById("map");
+
+
+if(!mapElement){
+
+return;
 
 }
 
-from
-
-"https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
 
+let map = L.map("map").setView(
 
-let map;
+[26.9124,75.7873],
 
-
-let marker;
-
-
-
-function createMap(){
-
-
-map = L.map("map").setView(
-
-[20.5937,78.9629],
-
-5
+13
 
 );
 
 
 
+
+
 L.tileLayer(
 
-"https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 
 {
 
-maxZoom:19
+attribution:
+"© OpenStreetMap"
 
 }
 
@@ -49,203 +46,49 @@ maxZoom:19
 
 
 
+return map;
+
+
 }
 
 
 
-createMap();
 
 
 
 
+// Add Marker
 
+export function addMarker(
 
-window.getCustomerLocation=function(){
+map,
+lat,
+lng,
+title
 
-
-
-navigator.geolocation.getCurrentPosition(
-
-(position)=>{
-
-
-let lat =
-position.coords.latitude;
-
-
-let lng =
-position.coords.longitude;
+){
 
 
 
-map.setView(
-
-[lat,lng],
-
-16
-
-);
-
-
-
-L.marker(
+let marker = L.marker(
 
 [lat,lng]
 
-)
-
-.addTo(map)
-
-.bindPopup(
-
-"You are here"
-
-)
-
-.openPopup();
+).addTo(map);
 
 
 
-}
-
-);
+if(title){
 
 
-};
-
-
-
-
-
-
-
-window.startRiderLocation=function(){
-
-
-
-navigator.geolocation.watchPosition(
-
-(position)=>{
-
-
-let lat =
-position.coords.latitude;
-
-
-let lng =
-position.coords.longitude;
-
-
-
-if(marker){
-
-marker.setLatLng(
-[lat,lng]
-);
-
-
-}
-
-else{
-
-
-marker =
-L.marker(
-[lat,lng]
-)
-
-.addTo(map);
-
+marker.bindPopup(title);
 
 
 }
 
 
 
-map.setView(
-
-[lat,lng],
-
-16
-
-);
-
-
-
-set(
-
-ref(
-
-db,
-
-"liveRider/location"
-
-),
-
-{
-
-latitude:lat,
-
-longitude:lng
-
-}
-
-);
-
-
-
-}
-
-);
-
-
-};
-
-
-
-
-
-
-// Show Rider Location For Customer
-
-
-const riderMap = document.getElementById("map");
-
-
-if(riderMap){
-
-
-onValue(
-
-ref(db,"liveRider/location"),
-
-(snapshot)=>{
-
-
-let data =
-snapshot.val();
-
-
-
-if(data && marker){
-
-
-marker.setLatLng(
-
-[
-data.latitude,
-data.longitude
-]
-
-);
-
-
-}
-
-
-}
-
-);
+return marker;
 
 
 }
