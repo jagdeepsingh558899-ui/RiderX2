e801110ed63e19e3d25827a65c75e2dc394e0b97
Jagdeep Singh <jagdeepsingh558899@gmail.com>
2026-08-07 14:56:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,24 @@
-const CACHE_NAME = "riderx-clean-v10";
+const CACHE_NAME = 'riderx-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/rider/index.html',
+  '/admin/dashboard.html',
+  '/assets/app.css',
+  '/assets/logo.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
 
-
-self.addEventListener("install", (event)=>{
-    self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-
-self.addEventListener("activate", (event)=>{
-
-    event.waitUntil(
-
-        caches.keys().then(keys=>{
-
-            return Promise.all(
-                keys.map(key=>caches.delete(key))
-            );
-
-        })
-
-    );
-
-    self.clients.claim();
-
-});
-
-
-
-self.addEventListener("fetch",(event)=>{
-
-    event.respondWith(
-
-        fetch(event.request)
-        .catch(()=>caches.match(event.request))
-
-    );
-
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
